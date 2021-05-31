0233ba00e08f9da11e51205dea45a39a16864e3c
tm <tm@blackberry.com>
2021-05-31 10:43:00 -0400

    delete

--- bst.c
+++ bst.c
@@ -21,7 +21,7 @@ fail:
     return new;
 }
 
-/* 
+/*
  * As root is getting modified in the
  * function, double pointer is needed.
  */
@@ -37,7 +37,7 @@ void insert (bst_t **root, int val)
     if (*root == NULL) {
         *root = new;
     } else {
-        
+
         bst_t *parent;
         while (temp) {
             parent = temp;
@@ -86,7 +86,7 @@ void postOrderTraversal(bst_t *root)
     printf("%d ", root->val);
 }
 
-/* 
+/*
  * https://www.geeksforgeeks.org/level-order-tree-traversal/
  *
  * BFS traversal
@@ -97,7 +97,7 @@ void postOrderTraversal(bst_t *root)
  *
  */
 
-#define Q_SZ  500  
+#define Q_SZ  500
 
 
 bst_t **creatQ(void)
@@ -143,9 +143,9 @@ void levelOrderTraversal(bst_t *root)
         printf("Unable to level traverse\n");
         return;
     }
-    
+
     while (temp) {
-        
+
         printf("%d ", temp->val);
 
         if (temp->left) {
@@ -190,10 +190,92 @@ void search(bst_t **root, int val)
  * case 2: No 2 children
  *          (i) No parent node (root node)
  *          (ii) Only one child
+ *
+ * 1. Find the node with the value
  */
 void delete(bst_t **root, int val)
 {
+    bst_t *temp = *root;
+    bst_t *parent = NULL;
 
+    /* 1. Find the Node */
+    while (temp) {
+        if (val < temp->val) {
+            parent = temp;
+            temp = temp->left;
+        } else if (val > temp->val) {
+            parent = temp;
+            temp = temp->right;
+        } else {
+            break; //found the value
+        }
+    }
+
+    if (!temp) {
+        printf("Value not found, %d\n", val);
+        return;
+    }
+
+    // temp is the candidate to be deleted
+
+    /* 2. Node to be deleted has atmost one child */
+    if (temp->left == NULL || temp->right == NULL) {
+
+        bst_t *newNode;
+
+        if (temp->left)
+            newNode = temp->left;
+        else
+            newNode = temp->right;
+
+        /* Check if the node to be deleted is the root */
+        if (parent == NULL) {
+            *root = newNode;
+            return;
+        }
+
+        /* Check the node to be attached is to the left or right of the parent */
+        if (parent->left == temp)
+            parent->left = newNode;
+        else
+            parent->right = newNode;
+
+
+        /* Free the node */
+        free(temp);
+    } else {
+
+        /* Node to be deleted has 2 children */
+
+
+        /* Compute the inorder successor: Rt sub tree's smallest node.
+         * In BST the left most node is guranteed to be smallest */
+        bst_t *inOrderSuccessor = temp->right;
+        parent = NULL;
+
+        while (inOrderSuccessor->left) {
+            parent = inOrderSuccessor;
+            inOrderSuccessor = inOrderSuccessor->left;
+        }
+printf("Inorder successor = %d\n", inOrderSuccessor->val);
+        // check if the parent of the inorder successor is the curr or not(i.e. curr= the node which has the same data as
+        // the given data by the user to be deleted). If it isn't, then make the left child of its parent equal to
+        // the inorder successor'd right child.
+        //
+        // If the inorder successor was the curr (i.e. curr = the node which has the same data as the given data by the
+        // user to be deleted), then make the right child of the node to be deleted equal to the right child of
+        // the inorder successor.
+
+        if (parent == NULL) {  //the first right node is the inorder successor
+            temp->right = inOrderSuccessor->right;
+        } else {
+            parent->left = inOrderSuccessor->right;
+        }
+
+        temp->val = inOrderSuccessor->val;
+
+        free(inOrderSuccessor);
+    }
 }
 
 
@@ -201,17 +283,19 @@ int main(void)
 {
     int i = 0;
     bst_t *root = NULL;
-    int arr[] = {10,4,15,2,5,13,22,1,14};
+    //int arr[] = {10,4,15,2,5,13,22,1,14};
+    int arr[] = {10,7,5,8,15,18};
 
     for (i = 0; i < (sizeof(arr)/sizeof(int)); i++) {
         insert(&root, arr[i]);
     }
-    
+
+#if 0
     insert(&root, 12);
 
     preOrderTraversal(root);
     printf("\n");
-    
+
     inOrderTraversal(root);
     printf("\n");
 
@@ -227,9 +311,24 @@ int main(void)
     search(&root,14);
     search(&root,16);
     search(&root,12);
+    levelOrderTraversal(root);
+    printf("\n");
 
     delete(&root, 12);
-    search(&root,12);
+    levelOrderTraversal(root);
+    printf("\n");
+
+    delete(&root, 10);
+    levelOrderTraversal(root);
+    printf("\n");
+#endif
+
+    levelOrderTraversal(root);
+    printf("\n");
+
+    delete(&root, 10);
+    levelOrderTraversal(root);
+    printf("\n");
 
     return 0;
 }
